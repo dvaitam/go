92fcf44d6882e0faa4faad5bf8b07221467479d4
Anil <amanchikatla@icloud.com>
2022-08-24 11:32:14 +0530

    56th day of Go

---
+++ 1000-1999/1200-1299/1290-1299/1293/1293b.go
@@ -0,0 +1,30 @@
+// 00
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func write_int(f *bufio.Writer, a int) {
+	f.Write([]byte(strconv.Itoa(a)))
+}
+func write_string(f *bufio.Writer, a string) {
+	f.Write([]byte(a))
+}
+
+func main() {
+	var n int
+	reader := bufio.NewReader(os.Stdin)
+	f := bufio.NewWriter(os.Stdout)
+	defer f.Flush()
+	fmt.Fscan(reader, &n)
+	ans := float64(0)
+	for n > 0 {
+		ans += float64(1) / float64(n)
+		n--
+	}
+	fmt.Println(ans)
+}
