dfab112b79d2d49cb0560271254416070aded51c
Anil <amanchikatla@icloud.com>
2022-12-20 09:58:24 +0530

    174th day of Go

--- 1000-1999/1500-1599/1590-1599/1594/1594c.go
+++ 1000-1999/1500-1599/1590-1599/1594/1594c.go
@@ -34,6 +34,40 @@ func main() {
 	defer f.Flush()
 	fmt.Fscan(reader, &T)
 	for t := 1; t <= T; t++ {
+		var n int
+		var c, s string
+		fmt.Fscan(reader, &n, &c, &s)
+		equal := true
+		for i := 0; i < n; i++ {
+			if s[i] != c[0] {
+				equal = false
+				break
+			}
+		}
+		// if T == 10000 {
+		// 	if t == 71 {
+		// 		fmt.Println(n, s, c)
+		// 	}
+		// } else {
+		if equal {
+			write(f, "0\n")
+		} else {
+			one := -1
+			for i := n / 2; i <= n; i++ {
+				if i*2 > n && s[i-1] == c[0] {
+					one = i
+					break
+				}
+			}
+			if one == -1 {
+				write(f, 2, "\n")
+				write(f, n-1, n, "\n")
+			} else {
+				write(f, 1, "\n")
+				write(f, one, "\n")
+			}
+			//	}
+		}
 
 	}
 }
