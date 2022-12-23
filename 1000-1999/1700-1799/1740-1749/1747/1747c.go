1076840ab5ae14ab48dc8363d8effc36029460ed
Anil <amanchikatla@icloud.com>
2022-12-23 12:41:14 +0530

    117th day of Go

---
+++ 1000-1999/1700-1799/1740-1749/1747/1747c.go
@@ -0,0 +1,58 @@
+// 00
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func write(f *bufio.Writer, a ...interface{}) {
+	f.Write([]byte(fmt.Sprint(a...)))
+}
+
+type Number interface {
+	int64 | float64 | int
+}
+
+func min[K Number](a K, b K) K {
+	if a < b {
+		return a
+	}
+	return b
+}
+func max[K Number](a K, b K) K {
+	if a > b {
+		return a
+	}
+	return b
+}
+func main() {
+	var T int
+	reader := bufio.NewReader(os.Stdin)
+	f := bufio.NewWriter(os.Stdout)
+	defer f.Flush()
+	fmt.Fscan(reader, &T)
+	for t := 1; t <= T; t++ {
+		var n int
+		fmt.Fscan(reader, &n)
+		a := make([]int, n)
+		mi := -1
+		for i := 0; i < n; i++ {
+			fmt.Fscan(reader, &a[i])
+			if i > 0 {
+				if mi == -1 {
+					mi = a[i]
+				} else {
+					mi = min(mi, a[i])
+				}
+			}
+		}
+		if a[0] > mi {
+			write(f, "Alice\n")
+		} else {
+			write(f, "Bob\n")
+		}
+
+	}
+}
