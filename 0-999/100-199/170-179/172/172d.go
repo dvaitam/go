fb9fabda27062c04c29ebe6d6390c4c1956e49eb
Anil <amanchikatla@icloud.com>
2023-01-02 14:54:19 +0530

    187th day of Go

--- 0-999/100-199/170-179/172/172d.go
+++ 0-999/100-199/170-179/172/172d.go
@@ -40,77 +40,20 @@ func main() {
 	f := bufio.NewWriter(os.Stdout)
 	defer f.Flush()
 	fmt.Fscan(reader, &a, &n)
-	if n == 5000000 && a == 5000000 {
-		write(f, "24674231279431\n")
-	} else {
 
-		ans := int64(0)
-		primes := make([]int, 4000)
-		primes[0] = 2
-		next_prime_index := 1
-		// small := map[int]int{}
-		// small[2] = 2
-		// small[1] = 1
-		// square := map[int]bool{}
-		// square[1] = true
-		largest_square := make([]int, a+n)
-		largest_square[1] = 1
-		if 2 < a+n {
-			largest_square[2] = 1
+	ans := int64(0)
+	d := make([]int, 11111111)
+	for i := 1; i*i < a+n; i++ {
+		ii := i * i
+		j := ii
+		for j < a+n {
+			d[j] = ii
+			j += ii
 		}
-		for i := 3; i < a+n; i++ {
-			prime := true
-			for j := 0; j < next_prime_index; j++ {
-				if i%primes[j] == 0 {
-					sq := primes[j] * primes[j]
-					if i%sq == 0 {
-						largest_square[i] = largest_square[i/sq] * sq
-					} else {
-						largest_square[i] = largest_square[i/primes[j]]
-					}
-					//small[i] = primes[j]
-					prime = false
-					break
-				}
-				if primes[j]*primes[j] > i {
-					break
-				}
-			}
-			if prime {
-				largest_square[i] = 1
-				if next_prime_index < 4000 {
-					primes[next_prime_index] = i
-					next_prime_index++
-				}
-
-				//primes = append(primes, i)
-				//small[i] = i
-			}
-			if i >= a {
-				ans += int64(i / largest_square[i])
-			}
-			// sq := math.Sqrt(float64(i))
-			// if int(sq)*int(sq) == i {
-			// 	square[i] = true
-			// }
-
-		}
-		for i := 1; i < 3; i++ {
-			if i >= a && i < a+n {
-				ans += int64(i / largest_square[i])
-			}
-		}
-		//fmt.Println("finished loop", len(primes))
-		//write(f, square, "\n")
-		//write(f, next_prime_index, "\n")
-
-		// for i := a; i < a+n; i++ {
-		// 	ans += int64(i / largest_square[i])
-		// }
-		// 	ans += int64(i / max_square)
-
-		// }
-		write(f, ans, "\n")
-		//fmt.Println(ans)
 	}
+	for i := a; i < a+n; i++ {
+		ans += int64(i / d[i])
+	}
+	write(f, ans, "\n")
+
 }
