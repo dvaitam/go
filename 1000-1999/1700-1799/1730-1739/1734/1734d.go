4434f517accd602aae1b0074470c8711139b6e11
Anil <amanchikatla@icloud.com>
2023-02-26 08:05:13 +0530

    242nd day of Go

--- 1000-1999/1700-1799/1730-1739/1734/1734d.go
+++ 1000-1999/1700-1799/1730-1739/1734/1734d.go
@@ -40,117 +40,83 @@ func main() {
 		k--
 		a := make([]int64, n)
 
-		minh := make([]int64, n)
-		rminh := make([]int64, n)
-
 		for i := 0; i < n; i++ {
 			fmt.Fscan(reader, &a[i])
-
-			if i == 0 {
-				minh[i] = -a[i]
+		}
+		na := make([]int64, 1)
+		na[0] = a[0]
+		j := 0
+		nk := k
+		for i := 1; i < n; i++ {
+			if na[j] < 0 && a[i] < 0 || na[j] >= 0 && a[i] >= 0 {
+				na[j] += a[i]
 			} else {
-				minh[i] = minh[i-1] - a[i]
-
-			}
-			if minh[i] < 0 {
-				minh[i] = 0
-			}
-		}
-
-		for i := n - 1; i >= 0; i-- {
-
-			if i == n-1 {
-				rminh[i] = -a[i]
-			} else {
-				rminh[i] = rminh[i+1] - a[i]
-			}
-			if rminh[i] < 0 {
-				rminh[i] = 0
-			}
-
-		}
-		possible := false
-		health := a[k]
-
-		i, j := k-1, k+1
-		if k == 0 || k == n-1 {
-			possible = true
-		} else {
-			if health >= minh[k-1] || health >= rminh[k+1] {
-				possible = true
-			}
-		}
-
-		//	write(f, minh, rminh, i, j, "\n")
-		for !possible && i >= 0 && j <= n-1 {
-			for a[i] >= 0 {
-				health += a[i]
-				if health >= minh[i-1] {
-					possible = true
-					break
-				}
-				i--
-			}
-			if possible {
-				break
-			}
-			for a[j] >= 0 {
-				health += a[j]
-				if health >= rminh[j+1] {
-					possible = true
-					break
-				}
+				na = append(na, a[i])
 				j++
 			}
-			if possible {
-				break
+			if i == k {
+				nk = j
 			}
-			//	write(f, health, minh[i], rminh[j], "\n")
-			if i >= 0 && health >= minh[i] || j <= n-1 && health >= rminh[j] {
-				possible = true
+		}
+
+		a = na
+		k = nk
+		n = len(na)
+		i, j := k, k
+		possible := true
+		health := a[k]
+		for true {
+			if i <= 0 || j >= n-1 {
 				break
 			}
 
-			left_health := health
-			starti := i
-			max_left_health := int64(0)
-			max_left_health_index := i
-			for a[starti]+left_health >= 0 {
-				left_health += a[starti]
-				if left_health >= max_left_health {
-					max_left_health = left_health
-					max_left_health_index = starti
+			startj := j + 1
+			energy_needed := int64(0)
+			health_gained := int64(0)
+			for startj < n {
+				health_gained += a[startj]
+				if health_gained < 0 {
+					energy_needed -= health_gained
+					health_gained = 0
+				}
+				if health_gained > energy_needed {
+					break
+				}
+				startj++
+			}
+
+			if health >= energy_needed && health_gained >= energy_needed {
+				j = startj
+				health += health_gained - energy_needed
+				continue
+			}
+			starti := i - 1
+
+			energy_needed_left := int64(0)
+			health_gained_left := int64(0)
+			for starti >= 0 {
+				health_gained_left += a[starti]
+				if health_gained_left < 0 {
+					energy_needed_left -= health_gained_left
+					health_gained_left = 0
+				}
+				if health_gained_left > energy_needed_left {
+					break
 				}
 				starti--
 			}
 
-			right_health := health
-			startj := j
-			max_right_health := int64(0)
-			max_right_health_index := j
-			for a[startj]+right_health >= 0 {
-				right_health += a[startj]
-				if right_health >= max_right_health {
-					max_right_health = right_health
-					max_right_health_index = startj
-				}
-				startj++
+			if health >= energy_needed_left && health_gained_left >= energy_needed_left {
+				i = starti
+				health += health_gained_left - energy_needed_left
+				continue
 			}
-			//write(f, health, max_left_health, max_right_health, max_left_health_index, max_right_health_index, i, j, "\n")
-
-			if i == max_left_health_index && j == max_right_health_index {
-				break
+			//write(f, health_gained, energy_needed, health_gained_left, energy_needed_left, "\n")
+			if health < energy_needed_left && health < energy_needed {
+				possible = false
 			}
-			if max_left_health > max_right_health {
-				health = max_left_health
-				i = max_left_health_index + 1
-			} else {
-				health = max_right_health
-				j = max_right_health_index + 1
-			}
+			break
 		}
-		//write(f, a, minh, rminh, "\n")
-
 		if possible {
 			write(f, "YES\n")
 		} else {
