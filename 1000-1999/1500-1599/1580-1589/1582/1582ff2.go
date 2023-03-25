6c6afe50918287dba41b74d93d7d66719a91411f
Anil <amanchikatla@icloud.com>
2023-03-25 17:59:06 +0530

    269th day of Go

---
+++ 1000-1999/1500-1599/1580-1589/1582/1582ff2.go
@@ -0,0 +1,91 @@
+// 00
+package main
+
+import (
+	"bufio"
+	"container/list"
+	"fmt"
+	"os"
+)
+
+func write(f *bufio.Writer, a ...interface{}) {
+	f.Write([]byte(fmt.Sprint(a...)))
+}
+
+type Number interface {
+	int64 | float64 | int
+}
+
+func min[K Number](a K, b K) K {
+	if a < b {
+		return a
+	}
+	return b
+}
+func max[K Number](a K, b K) K {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+var table [][]bool
+var upd []*list.List
+var updl []int
+
+func setv(a int, b int) {
+	if !table[a][b] {
+		table[a][b] = true
+		if a > 0 {
+			setv(a-1, b)
+		}
+		upd[a].PushBack(b)
+	}
+}
+func roll(a int) {
+	for upd[a].Len() > 0 {
+		last := upd[a].Back().Value.(int)
+		setv(a-1, last^a)
+		back := upd[a].Back()
+		upd[a].Remove(back)
+	}
+}
+func main() {
+	var n int
+	reader := bufio.NewReader(os.Stdin)
+	f := bufio.NewWriter(os.Stdout)
+	defer f.Flush()
+	fmt.Fscan(reader, &n)
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &a[i])
+	}
+	table = make([][]bool, 5001)
+	upd = make([]*list.List, 5001)
+	updl = make([]int, 8192)
+
+	for i := 0; i < 5001; i++ {
+		table[i] = make([]bool, 8192)
+		upd[i] = list.New()
+	}
+	for i := 0; i <= 5000; i++ {
+		setv(i, 0)
+	}
+	for i := n - 1; i >= 0; i-- {
+		if a[i] > 0 {
+			roll(a[i])
+		}
+	}
+	ans := make([]int, 0)
+	for i := 0; i < 8192; i++ {
+		if table[0][i] {
+			ans = append(ans, i)
+		}
+	}
+	write(f, len(ans), "\n")
+	for i := 0; i < len(ans); i++ {
+		write(f, ans[i], " ")
+	}
+	write(f, "\n")
+
+}
