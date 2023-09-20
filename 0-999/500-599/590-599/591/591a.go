d071478214255b0d7cdcca1ee590a6e7bca15920
Anil <amanchikatla@icloud.com>
2023-09-20 08:30:03 +0530

    437th day of Go

---
+++ 0-999/500-599/590-599/591/591a.go
@@ -0,0 +1,43 @@
+// 00
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func write(f *bufio.Writer, a ...interface{}) {
+	f.Write([]byte(fmt.Sprint(a...)))
+}
+
+type Number interface {
+	int64 | float64 | int
+}
+
+func min[K Number](a K, b K) K {
+	if a < b {
+		return a
+	}
+	return b
+}
+func max[K Number](a K, b K) K {
+	if a > b {
+		return a
+	}
+	return b
+}
+func abs[K Number](a K) K {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+func main() {
+	var l, p, q int
+	reader := bufio.NewReader(os.Stdin)
+	f := bufio.NewWriter(os.Stdout)
+	defer f.Flush()
+	fmt.Fscan(reader, &l, &p, &q)
+	write(f, float64(l*p)/float64(p+q), "\n")
+}
