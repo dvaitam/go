42aa410f9051d3e1168d996a1183ca00145b0c64
Anil <amanchikatla@icloud.com>
2023-09-28 08:11:59 +0530

    445th day of Go

---
+++ 1000-1999/1800-1899/1860-1869/1869/1869a.go
@@ -0,0 +1,61 @@
+// 00
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func write(f *bufio.Writer, a ...interface{}) {
+	f.Write([]byte(fmt.Sprint(a...)))
+}
+
+type Number interface {
+	int64 | float64 | int
+}
+
+func min[K Number](a K, b K) K {
+	if a < b {
+		return a
+	}
+	return b
+}
+func max[K Number](a K, b K) K {
+	if a > b {
+		return a
+	}
+	return b
+}
+func abs[K Number](a K) K {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+func main() {
+	var T int
+	reader := bufio.NewReader(os.Stdin)
+	f := bufio.NewWriter(os.Stdout)
+	defer f.Flush()
+	fmt.Fscan(reader, &T)
+	for t := 1; t <= T; t++ {
+		var n int
+		fmt.Fscan(reader, &n)
+		a := make([]int, n)
+		for i := 0; i < n; i++ {
+			fmt.Fscan(reader, &a[i])
+		}
+		if n%2 == 0 {
+			write(f, "2\n")
+			write(f, 1, n, "\n")
+			write(f, 1, n, "\n")
+		} else {
+			write(f, "4\n")
+			write(f, 1, n, "\n")
+			write(f, 1, n-1, "\n")
+			write(f, 2, n, "\n")
+			write(f, 2, n, "\n")
+		}
+	}
+}
