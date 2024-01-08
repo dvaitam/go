be63bb229b5ca7ae3eee4473dbe9a0df9ca1422b
Anil <amanchikatla@icloud.com>
2024-01-08 07:09:01 +0530

    455th day of Go

---
+++ 0-999/0-99/60-69/69/69a.go
@@ -0,0 +1,55 @@
+// 00
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func write(f *bufio.Writer, a ...interface{}) {
+	f.Write([]byte(fmt.Sprint(a...)))
+}
+
+type Number interface {
+	int64 | float64 | int
+}
+
+func min[K Number](a K, b K) K {
+	if a < b {
+		return a
+	}
+	return b
+}
+func max[K Number](a K, b K) K {
+	if a > b {
+		return a
+	}
+	return b
+}
+func abs[K Number](a K) K {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+func main() {
+	var T int
+	reader := bufio.NewReader(os.Stdin)
+	f := bufio.NewWriter(os.Stdout)
+	defer f.Flush()
+	fmt.Fscan(reader, &T)
+	var a, b, c int
+	for t := 1; t <= T; t++ {
+		var x, y, z int
+		fmt.Fscan(reader, &x, &y, &z)
+		a += x
+		b += y
+		c += z
+	}
+	if a == 0 && b == 0 && c == 0 {
+		write(f, "YES\n")
+	} else {
+		write(f, "NO\n")
+	}
+}
